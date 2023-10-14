ccaa81d7a271298018ab990efc77adcd9f29a0f7
f4t4nt <nishant@bhakars.com>
2023-10-14 09:39:43 -0700

    rough work on conditions

--- probability.py
+++ probability.py
@@ -42,12 +42,37 @@ def clean_string(s, remove_spaces=True):
         s = re.sub(r'\s+', '', s)
     return s
 
-def synonyms(bank):
+def check_chars(word, fixed):
+    assert fixed is not None
+    for i, c in enumerate(word):
+        if not c in fixed[i]:
+            return False
+    return True
+
+def apply_cond(word, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
+    if exact_len:
+        min_len = exact_len
+        max_len = exact_len
+    if not fixed:
+        tmp = {}
+        for c in 'abcdefghijklmnopqrstuvwxyz':
+            tmp[c] = 1
+        fixed = [tmp] * max_len
+    return len(word) >= min_len \
+        and len(word) <= max_len \
+        and (not fixed or check_chars(word, fixed)) \
+        and (not in_dict or word in cw_dict)
+
+def synonyms(bank, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
     if isinstance(bank, list):
         bank = ' '.join(bank)
     preds, preds_scores = models.answer_clues(dpr, [bank], 999999999, output_strings=True)
     preds, preds_scores = preds[0], list(preds_scores[0])
-    preds = [clean_string(p, remove_spaces=False) for p in preds]
+    preds = [clean_string(p) for p in preds]
+    new_preds = [p for p in preds if apply_cond(p, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict)]
+    new_preds_scores = [preds_scores[i] for i, p in enumerate(preds) if apply_cond(p, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict)]
+    preds = new_preds
+    preds_scores = new_preds_scores
     preds_map = {}
     max_score = max(preds_scores)
     score_sum = np.sum(np.exp(np.array(preds_scores) - max_score))
@@ -72,14 +97,14 @@ class Operator:
             for w, w_s in self.bank[i].items():
                 for b, b_s in self.all_banks(i + 1):
                     yield ([w] + b, w_s * b_s)
-        
-    def eval(self):
+    
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
         new_bank = []
         for b in self.bank:
             if isinstance(b, dict):
                 new_bank.append(b)
             else:
-                new_bank.append(b.eval())
+                new_bank.append(b.eval(exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict)) # TODO: fix this
         self.bank = new_bank
         return {}
 
@@ -87,72 +112,78 @@ class Alternation(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
         super().eval()
-        options = {}
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join(b))
             for i in range(len(merged)):
                 cur = merged[i]
                 for j in range(i + 2, len(merged), 2):
                     cur += merged[j]
-                    if len(cur) >= 3: # for now
-                        if not cur in options:
-                            options[cur] = 0
-                        options[cur] += b_s
-        return options
+                    if apply_cond(cur, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                        if not cur in self.options:
+                            self.options[cur] = 0
+                        self.options[cur] += b_s
+        return self.options
 
 class Anagram(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
-        super().eval()
-        options = {}
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
+        super().eval(max_len=max_len)
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join(b))
-            assert len(merged) <= 15 # for now
+            if len(merged) > max_len or len(merged) < min_len:
+                continue
             for perm in itertools.permutations(merged):
                 cur = ''.join(perm)
-                if not cur in options:
-                    options[cur] = 0
-                options[cur] += b_s
-        return options
+                if apply_cond(cur, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                    if not cur in self.options:
+                        self.options[cur] = 0
+                    self.options[cur] += b_s
+        return self.options
     
 class Concatenation(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
-        super().eval()
-        options = {}
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
+        super().eval(max_len=max_len)
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join(b))
-            if not merged in options:
-                options[merged] = 0
-            options[merged] += b_s
-        return options
+            if len(merged) > max_len:
+                continue
+            if not merged in self.options:
+                self.options[merged] = 0
+            self.options[merged] += b_s
+        return self.options
 
 class Container(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
-        super().eval()
-        options = {}
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
+        super().eval(max_len=max_len)
+        self.options = {}
         for b, b_s in self.all_banks():
             assert len(b) == 2 # for now
             for i in range(len(b[0]) + 1):
                 cur = b[0][:i] + b[1] + b[0][i:]
-                if not cur in options:
-                    options[cur] = 0
-                options[cur] += b_s
+                if apply_cond(cur, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                    if not cur in self.options:
+                        self.options[cur] = 0
+                    self.options[cur] += b_s
             for i in range(len(b[1]) + 1):
                 cur = b[1][:i] + b[0] + b[1][i:]
-                if not cur in options:
-                    options[cur] = 0
-                options[cur] += b_s
-        return options
+                if apply_cond(cur, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                    if not cur in self.options:
+                        self.options[cur] = 0
+                    self.options[cur] += b_s
+        return self.options
 
 # not really sure if this is distinct to other ops..
 # https://en.wikipedia.org/wiki/Cryptic_crossword#Deletions
@@ -169,47 +200,49 @@ class Hidden(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
         super().eval()
-        options = {}
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join(b))
             for i in range(len(merged)):
                 for j in range(i + 1, len(merged)): # need to enforce using all words
                     cur = merged[i:j]
-                    if len(cur) >= 3: # for now
-                        if not cur in options:
-                            options[cur] = 0
-                        options[cur] += b_s
-        return options
+                    if apply_cond(cur, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                        if not cur in self.options:
+                            self.options[cur] = 0
+                        self.options[cur] += b_s
+        return self.options
     
 class Initialism(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
         super().eval()
-        options = {}
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join([b_[0] for b_ in b]))
-            if not merged in options:
-                options[merged] = 0
-            options[merged] += b_s
-        return options
+            if apply_cond(merged, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                if not merged in self.options:
+                    self.options[merged] = 0
+                self.options[merged] += b_s
+        return self.options
     
 class Terminalism(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
         super().eval()
-        options = {}
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join([b_[-1] for b_ in b]))
-            if not merged in options:
-                options[merged] = 0
-            options[merged] += b_s
-        return options
+            if apply_cond(merged, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                if not merged in self.options:
+                    self.options[merged] = 0
+                self.options[merged] += b_s
+        return self.options
     
 class Homophone(Operator):
     def __init__(self, bank):
@@ -234,46 +267,50 @@ class Reversal(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
-        super().eval()
-        options = {}
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
+        super().eval(max_len=max_len)
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = clean_string(''.join(b))[::-1]
-            if not merged in options:
-                options[merged] = 0
-            options[merged] += b_s
-        return options
+            if apply_cond(merged, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                if not merged in self.options:
+                    self.options[merged] = 0
+                self.options[merged] += b_s
+        return self.options
 
 class Substitution(Operator):
     def __init__(self, bank):
         super().__init__(bank)
         
-    def eval(self):
+    def eval(self, exact_len=None, min_len=1, max_len=100, fixed=None, in_dict=False):
         super().eval()
-        options = {}
+        self.options = {}
         for b, b_s in self.all_banks():
             merged = ' '.join(b)
-            syns = synonyms(merged)
+            syns = synonyms(merged, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict)
             for syn, syn_s in syns.items():
-                syn = clean_string(syn)
-                if not syn in options:
-                    options[syn] = 0
-                options[syn] += b_s * syn_s
-        options = sorted(options.items(), key=lambda x: -x[1])
-        options = options[:1000]
-        options = {o[0]: o[1] for o in options}
-        return options
+                # syn = clean_string(syn) # should already be clean
+                if apply_cond(syn, exact_len=exact_len, min_len=min_len, max_len=max_len, fixed=fixed, in_dict=in_dict):
+                    if not syn in self.options:
+                        self.options[syn] = 0
+                    self.options[syn] += b_s * syn_s
+        self.options = sorted(self.options.items(), key=lambda x: -x[1])
+        self.options = self.options[:1000] # for now
+        self.options = {o[0]: o[1] for o in self.options}
+        return self.options
 
 class Definition:
     def __init__(self, defn):
         self.defn = defn
     
-    def eval(self):
-        return synonyms(self.defn)
+    def eval(self, exact_len=None, max_len=100):
+        if exact_len:
+            max_len = exact_len
+        return synonyms(self.defn, max_len=max_len)
 
 def eval(part1, part2, ans):
-    term1 = part1.eval().get(ans, 0)
-    term2 = part2.eval().get(ans, 0)
+    term1 = part1.eval(exact_len=len(ans)).get(ans, 0)
+    term2 = part2.eval(exact_len=len(ans)).get(ans, 0)
     print(term1, "*", term2, "=", term1 * term2)
     return term1 * term2
 
